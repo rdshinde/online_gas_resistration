01679eb10cbb8d5afa4b2154599ba2b5cc8c8dec
Rishikesh Shinde <67017632+rdshinde@users.noreply.github.com>
2021-12-14 18:07:20 +0530

    Update dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -3,7 +3,7 @@
 $serverName = "localhost";
 $dbUsername = "root";
 $dbPassword = "";
-$dbName = "online_gas_booling";
+$dbName = "online_gas_booking";
 
 $conn = mysqli_connect($serverName, $dbUsername, $dbPassword, $dbName);
 
