b13f025217d87b76941835d41b18011bee9e9ee3
Rishikesh Shinde <67017632+rdshinde@users.noreply.github.com>
2021-12-14 18:03:11 +0530

    Update dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,9 +1,9 @@
 <?php
 
 $serverName = "localhost";
-$dbUsername = "id18109467_root";
-$dbPassword = "h+%}Jal7f141*Du1";
-$dbName = "id18109467_online_gas_booling";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "online_gas_booling";
 
 $conn = mysqli_connect($serverName, $dbUsername, $dbPassword, $dbName);
 
